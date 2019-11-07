3dff02e0d571b83129cb7df6a4a3545f17de6a97
Vamp <vampiirecodes@gmail.com>
2019-11-06 19:58:43 -0500

    API: fix handler import

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const { handleFormSubmission } = require("./handlers");
+const { formSubmitHandler } = require("./handlers");
 const { allowCORS, enforceJSON } = require("./middleware");
 
 const app = express();
@@ -15,7 +15,7 @@ app.get("/form", (_, res) => res.send("use the POST, Luke."));
 // first run the enforceJSON middleware (see middleware.js for notes)
 // then use the express.json middleware to process the request packets into JSON then parse it into an object attached on req.body
 // finally (if both middleware functions succeed) pass the request to the form submission handler (see handlers.js for notes)
-app.post("/form", enforceJSON, express.json(), handleFormSubmission);
+app.post("/form", enforceJSON, express.json(), formSubmitHandler);
 
 // extract the PORT variable from the environment
 // or (if not present) default to a value of 3000
